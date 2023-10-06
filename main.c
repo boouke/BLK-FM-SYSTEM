4388ad5138494b5c3d7f7330f15d1b04f2698397
boouke <119793653+boouke@users.noreply.github.com>
2023-10-06 23:01:36 +0200

    Update main.c
    fix dir perms

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #define DELETE_MSG "You have selected deleting mode! Type in the files current adress starting at the root folder and it will be deleted!\n"
 #define SUCCESS_MSG "Operation succesful!\n"
 #define RENAME_MSG "You have selected renaming mode! Type in the files current adress starting at the root folder and name and a new name for it!\n"
-#define ERR_LEN "UNKNOWN ERROR(0x1) Please contact our support team ar github.com∕boouke and provide the memory adress\n"
+#define ERR_LEN "UNKNOWN ERROR(0x1) Please contact our support team ar github.com/boouke and provide the memory adress\n"
 #define ERR_FILE_CREATE "FILE CREATE ERROR(0x2) Please restart the program, and try again. If you encounter the same problem contact our support team at github.com/boouke\n"
 
 void choose(int* int_operation);
@@ -104,36 +104,48 @@ void file_delete(void){
 }
 void dir_create(void) {
     int len = 50;
-    char perms[10];
     char* dir_name = (char*)malloc((len + 1) * sizeof(char));
-    printf("%s", DIR_CREATE_MSG);
+    printf("Enter the directory name: ");
+
     while (scanf("%s", dir_name) != 1) {
-        printf("%s at %p", ERR_LEN, dir_name);
+        printf("Invalid input. Please enter a valid directory name: ");
     }
-    printf("What permissions do you want to have? You can select all, read or write\n");
-    while(scanf("%s", perms) != 1 && strcmp(perms, "all") != 0 && strcmp(perms, "write") != 0 && strcmp(perms, "read") != 0 && strcmp(perms, "r&w") != 0){
-        printf("%s", ERR_LEN);
-    }
-    if(strcmp(perms, "all")){
-        strcpy(perms, "S_IRWXU");
-    }
-    else if(strcmp(perms, "read")){
-        strcpy(perms, "S_IWUSR");
-    }
-    else if(strcmp(perms, "write")){
-        strcpy(perms, "S_IXUSR");
-    }
-    int creating = mkdir(dir_name, perms);
-    if(creating == 0){
-        printf("%s", SUCCESS_MSG);
+
+    // Create the directory
+    int creating = mkdir(dir_name);
+
+    if (creating == 0) {
+        printf("Directory \"%s\" created successfully.\n", dir_name);
+    } else {
+        printf("Error creating directory \"%s\".\n", dir_name);
+        free(dir_name);
         return;
     }
-    else{
-        printf("%s at %p", ERR_LEN, dir_name);
-        return;
+
+    int others_perms;
+
+    printf("Set permissions for others (1 = Read-Only, 2 = Read-Write): ");
+    scanf("%d", &others_perms);
+
+    int permission_mode = 0;
+
+    // Convert user selections to permission bits
+    if (others_perms == 2) {
+        permission_mode |= S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+    } else {
+        permission_mode |= S_IRUSR | S_IRGRP | S_IROTH;
     }
+
+    if (chmod(dir_name, permission_mode) != 0) {
+        printf("Failed to set permissions for %s\n", dir_name);
+    } else {
+        printf("Permissions set successfully for %s\n", dir_name);
+    }
+
     free(dir_name);
 }
+
+
 
 void name(void){
     int len = 50;
